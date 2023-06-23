011bbde4977bd1fdb03fb1097258fbdc712d5eb3
amaral <127356902+thxtchyy@users.noreply.github.com>
2023-06-23 16:46:20 -0300

    testeinterface1.py

--- testeinterface1.py
+++ testeinterface1.py
@@ -1,14 +1,12 @@
 # Para importar o Tkinter para o seu projeto basta digitar
 from tkinter import *
 
-# Agora, para criar uma nova instância, crie uma variável 
-# com o nome "janela" e atribua a ela a instância usando "Tk()":
+# PARA CRIAR UMA NOVA INSTÂNCIA -> CRIAR VARIÁVEL. 
+# variável: Janela; instância: Tk()
 janela = Tk()
 
-# Para adicionar algum texto, crie o Label, vincule ele a 
-# variável janela e adicione o texto de seu interesse. 
-# (Não esqueça de adicionar o gerenciador de layout)
+# o label deve estár vinculado a instância.
 Label(janela, text="olá, mundo").pack()
 
-# E mande a aplicação ser executada
+# faz a janela continuar aberta.
 janela.mainloop() 
